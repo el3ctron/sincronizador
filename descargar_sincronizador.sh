ed9efa7f68d76e7ccc1df663c65dd6f625c98231
el3ctron <el3ctron@cryptolab.net>
2020-07-23 18:46:55 -0500

    estandarización de rutas y mejoras de interfaz

--- descargar_sincronizador.sh
+++ descargar_sincronizador.sh
@@ -5,6 +5,8 @@ echo
 sudo apt-get install -y git python-pip build-essential
 # sudo apt-get install -y python3-pip python-setuptools python-dev 
 sudo pip install percol
+echo "## ######################################"
+echo ""
 echo -n " [ digita tu nombre de usuario asignado por la comisión ] : "
 read usuario
 echo -n " [ digita la ip asignada ] : "
@@ -14,6 +16,8 @@ read puerto
 repo=/rep/git/sincronizador
 cd
 
+echo ""
+echo "## ######################################"
 echo ""
 echo " [ INICIANDO DESCARGA DEL SINCRONIZADOR ]"
 echo ""
@@ -29,12 +33,15 @@ echo ""
 ## ######################################
 
 echo ""
-echo " - a continuación aparecerá un menú que se puede operar con las flechas del teclado y enter,"
-echo "   el menú cuenta con números para señalar el orden de ejecución de los pasos para iniciar la sincronización de archivos."
+echo " A continuación aparecerá un menú que se puede operar con"
+echo " las flechas del teclado y enter."
+echo " El menú cuenta con números para señalar el orden de ejecución"
+echo " de los pasos para iniciar la sincronización de archivos."
 echo ""
-
+echo "## ######################################"
+echo ""
 read -n 1 -s -r -p "[Presiona una tecla para continuar] "
 
-echo $ip > ~/sincronizador/.ip
-echo $usuario > ~/sincronizador/.usuario
-~/sincronizador/.sincronizador
+echo $ip > ./sincronizador/.ip
+echo $usuario > ./sincronizador/.usuario
+./sincronizador/.sincronizador
