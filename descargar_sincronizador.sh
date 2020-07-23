b150edcd166ee4d6bafb379dab0ae389735007f8
el3ctron <el3ctron@cryptolab.net>
2020-07-23 17:25:33 -0500

    mejoras en el sincronizador

--- descargar_sincronizador.sh
+++ descargar_sincronizador.sh
@@ -1,21 +1,29 @@
 ## ######################################
 ## ######################################
 
-sudo apt-get install -y git python-pip python3-pip python-setuptools python-dev build-essential
+echo
+sudo apt-get install -y git python-pip build-essential
+# sudo apt-get install -y python3-pip python-setuptools python-dev 
 sudo pip install percol
 echo -n " [ digita tu nombre de usuario asignado por la comisión ] : "
 read usuario
-#usuario=$(echo ./.usuario)
-ip=$(echo ./.ip)
+echo -n " [ digita la ip:puerto asignado ] : "
+read ip
 repo=/rep/
 cd
+echo git clone ssh://$usuario@$ip$repo
 git clone ssh://$usuario@$ip$repo
 cd sincronizadores
+echo $ip ./.ip
+echo $usuario ./.usuario
 ./.sincronizador
 
 ## ######################################
 ## ######################################
 
-echo "a continuación aparecerá un menú que se puede operar con las flechas del teclado y enter;"
-echo "este menú se encuentra en un orden numérico fácil de entender."
+echo ""
+echo " - a continuación aparecerá un menú que se puede operar con las flechas del teclado y enter,"
+echo "   el menú está en un orden numérico fácil de entender."
+echo ""
+
 read -n 1 -s -r -p "[Presiona una tecla para continuar] "
